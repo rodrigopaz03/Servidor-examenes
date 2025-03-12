0173c672e2486cc69339173596f0997e02a87219
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-03-11 23:42:54 -0500

    cambios

--- server1/server1_project/settings.py
+++ server1/server1_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e=&&$50$0a+1^up7ud@yzdkszt!+=s%g+-m)m1vs-xvn%o35wc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
