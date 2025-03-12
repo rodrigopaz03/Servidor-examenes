f84c902cf5ec49b1327a2ec0eda3a0f0a30899b7
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-03-11 23:38:01 -0500

    cambios

--- server1/uploads/models.py
+++ server1/uploads/models.py
@@ -1,10 +1,9 @@
 from django.db import models
 
-
 class Imagen(models.Model):
     titulo = models.CharField(max_length=100, blank=True)
-    imagen_binaria = models.BinaryField()  # Guarda el contenido binario
-    content_type = models.CharField(max_length=50, blank=True, null=True)  # Opcional: tipo de archivo (e.g., "image/jpeg")
+    imagen_binaria = models.BinaryField()  
+    content_type = models.CharField(max_length=50, blank=True, null=True)
     fecha_subida = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
