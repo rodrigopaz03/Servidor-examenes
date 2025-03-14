04562a141bd254ebd25f5e1d8b06f0dff98405b2
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-03-13 23:52:44 -0500

    cambios

--- server1/uploads/models.py
+++ server1/uploads/models.py
@@ -2,19 +2,14 @@ from django.db import models
 
 class Imagen(models.Model):
     titulo = models.CharField(max_length=100, blank=True)
-    imagen_binaria = models.BinaryField(null=True, blank=True)
+    url_gcp = models.URLField(null=True, blank=True)
     content_type = models.CharField(max_length=50, blank=True, null=True)
     fecha_subida = models.DateTimeField(auto_now_add=True)
-    ensamblada = models.BooleanField(default=False)  # Indica si ya se reensambló la imagen
 
     def __str__(self):
         return self.titulo or f"Imagen {self.pk}"
 
 class ImagenChunk(models.Model):
-    imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE, related_name="chunks")
-    chunk_index = models.IntegerField()  # Orden del chunk (0-based)
-    chunk_data = models.BinaryField()      # Datos binarios del chunk
-
-    class Meta:
-        unique_together = ('imagen', 'chunk_index')
-        ordering = ['chunk_index']
+    imagen = models.ForeignKey(Imagen, on_delete=models.CASCADE, related_name='chunks')
+    chunk_index = models.IntegerField()
+    archivo_chunk = models.BinaryField()
