1705e5c2248d4725ca1379ad83a87186e5f1cb53
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-03-14 01:54:49 -0500

    cambio

--- server1/uploads/models.py
+++ server1/uploads/models.py
@@ -5,6 +5,7 @@ class Imagen(models.Model):
     imagen_binaria = models.BinaryField(null=True, blank=True)
     content_type = models.CharField(max_length=50, blank=True, null=True)
     fecha_subida = models.DateTimeField(auto_now_add=True)
+    ensamblada = models.BooleanField(default=False)
 
     def __str__(self):
         return self.titulo or f"Imagen {self.pk}"
