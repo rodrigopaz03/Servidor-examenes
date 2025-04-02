fc0eca801017d71fa7fba5a76ba0bbc28bbed985
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-04-02 14:31:45 -0500

    nueva ip

--- server1/server1_project/settings.py
+++ server1/server1_project/settings.py
@@ -96,7 +96,7 @@ DATABASES = {
         'NAME': 'db-hospital',
         'USER': 'db-user',
         'PASSWORD': 'ISIS2503',
-        'HOST': '10.65.176.3',
+        'HOST': '10.65.176.7',
         'PORT': '5432',
 
     }
