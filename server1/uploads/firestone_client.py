2e57cc10ab3d044b16381bb738bd02f23447cfb9
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-24 23:22:21 -0500

    correccion

--- server1/uploads/firestone_client.py
+++ server1/uploads/firestone_client.py
@@ -3,5 +3,5 @@ from django.conf import settings
 
 db = firestore.Client(
     project="exalted-booster-454620-j9",
-    database="projects/exalted-booster-454620-j9/databases/db-imagenes"
+    database="db-imagenes"
 )
