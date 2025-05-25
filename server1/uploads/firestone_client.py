df4dda668e35885f4c0c744970590fe78eb94c55
rodrigopaz03 <r.pazl@uniandes.edu.co>
2025-05-24 23:20:06 -0500

    nueva db

--- server1/uploads/firestone_client.py
+++ server1/uploads/firestone_client.py
@@ -1,4 +1,7 @@
 from google.cloud import firestore
 from django.conf import settings
 
-db = firestore.Client(project=settings.GOOGLE_CLOUD_PROJECT)
+db = firestore.Client(
+    project="exalted-booster-454620-j9",
+    database="projects/exalted-booster-454620-j9/databases/db-imagenes"
+)
