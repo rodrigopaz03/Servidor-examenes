a12c78de355ca663566b911443a11aa0880d5bf5
juanjomurillo12 <j.murilloa@uniandes.edu.co>
2025-05-28 23:45:25 -0500

    cc

--- server1/server1_project/settings.py
+++ server1/server1_project/settings.py
@@ -17,7 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SERVER2_URL = 'http://34.172.1.19:8080/registro/'
+SERVER2_URL = 'http://130.211.118.196/registro/'
 GOOGLE_CLOUD_PROJECT = "exalted-booster-454620-j9"
 
 
